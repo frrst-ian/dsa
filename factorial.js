bc70dcf3e1de66f1bcc13bfc2c43c887f49abca1
frrst-ian <frrstxd@proton.me>
2024-08-12 20:32:03 +0800

    Complete the project

---
+++ factorial.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/* eslint-disable no-undef */
+function fibs(n) {
+    let array = [];
+
+    array[0] = 0;
+    array[1] = 1;
+
+    for (let i = 2; i < n; i++) {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+
+    return array;
+}
+
+let n = 8
+let answer = fibs(n)
+
+console.log(answer)
+function fibsRec(n) {
+    console.log("This was printed recursively")
+
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    let prev = fibsRec(n - 1);
+    prev.push(prev[prev.length - 1] + prev[prev.length - 2]);
+    return prev;
+}
+
+answer = fibsRec(n);
+console.log(answer);
